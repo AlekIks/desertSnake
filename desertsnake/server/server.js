735d1512cfa081e8f7855f4bb3748da740f6d671
AlekIks <ammalium@mail.ru>
2020-03-30 03:55:02 +0300

    если оно заработает, я удивлюсь

--- desertsnake/server/server.js
+++ desertsnake/server/server.js
@@ -3,8 +3,8 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use(express.static('desertSnake/static'))
-app.use(express.static(path.join(__dirname, 'desertSnake/static/css')));
+app.use(express.static('desertsnake/scr/'))
+app.use(express.static(path.join(__dirname, 'desertsnake/scr')));
 
 app.listen(process.env.PORT || 8080, ()=> {
     console.log('Server started')
